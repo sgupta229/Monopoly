0264f111497b7533ce9ab26de78eda6f968529e7
Ryan Bloom <rab83@duke.edu>
2019-04-01 22:13:32 -0400

    added example_data.xml and an empty DESIGN_PLAN.md

--- src/Controller/Board.java
+++ src/Controller/Board.java
@@ -2,7 +2,9 @@ package Controller;
 
 import Model.Space;
 
+import java.security.InvalidParameterException;
+
 public interface Board {
-    public Space getSpaceAt(int location);
-    public int getLocationOfSpace(Space space);
+    public Space getSpaceAt(int location) throws IndexOutOfBoundsException;
+    public int getLocationOfSpace(Space space) throws InvalidParameterException;
 }
