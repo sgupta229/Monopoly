a45cbb66cbffe51d2551d6729257614af320935f
Ryan Bloom <rab83@duke.edu>
2019-04-01 21:22:34 -0400

    added interfaces in controller/model.  Filled in method signatures for several interfaces

---
+++ src/Controller/Dice.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public interface Dice {
+    public int rollDice(int sides);
+}
---
+++ src/Model/ActionCard.java
@@ -0,0 +1,7 @@
+package Model;
+
+import Controller.Game;
+
+public interface ActionCard {
+    public void doAction(Game game);
+}
