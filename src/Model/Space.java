796dc5be3309f0ba04db5fd5beb6f01eae335435
DYLAN KARUL <djk41@duke.edu>
2019-04-06 15:18:14 -0400

    made income tax space and its action

--- src/Model/Space.java
+++ src/Model/Space.java
@@ -7,7 +7,9 @@ import java.util.*;
  * that can be on the board (a property space, jail space, free parking, chance space, etc)
  * It will hold a list of which players (if any) are on itself
  */
-public interface Space {
+public abstract class Space {
+
+    List<Player> myOccupants = new ArrayList<>();
 
     /***
      * This method performs the specific action that a type of space requires.
@@ -16,19 +18,33 @@ public interface Space {
      * get a specific deck and draw a card, and more.
      * @param game the active Game driver class for this game
      */
-    public void doAction(Game game);
+    public abstract void doAction(Game game);
 
     /***
      * Getter method that gets all the players on the given space
      * @return the list of Occupants
      */
-    public List<Player> getOccupants();
+    public List<Player> getOccupants(){
+        return myOccupants;
+    }
 
     /***
      * adds a player to the list of players on the space
      * @param newOccupant the player that is now on the spot
      */
-    public void addOccupant(Player newOccupant);
+    public void addOccupant(Player newOccupant){
+        if(!myOccupants.contains(newOccupant)){
+            myOccupants.add(newOccupant);
+        }
+    }
 
-    public void removeOccupant(Player occupantToRemove);
+    /***
+     * removes a player to the list of players on the space
+     * @param occupantToRemove the player that has left the spot
+     */
+    public void removeOccupant(Player occupantToRemove){
+        if(myOccupants.contains(occupantToRemove)){
+            myOccupants.remove(occupantToRemove);
+        }
+    }
 }
