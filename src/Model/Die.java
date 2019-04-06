b00f430e41d115de75b23e3a65c6da2769946bfe
Sahil Gupta <sg390@duke.edu>
2019-04-06 19:27:21 -0400

    Added dice functionality

---
+++ src/Model/Die.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Die {
+    private int numSides;
+    private int[] values;
+
+    public Die(int numSides, int[] values) {
+        this.numSides = numSides;
+        this.values = values;
+    }
+
+    public int rollDie() {
+        int max = values.length - 1;
+        int range = max + 1;
+        int index = (int)(Math.random() * range);
+        return values[index];
+    }
+}
