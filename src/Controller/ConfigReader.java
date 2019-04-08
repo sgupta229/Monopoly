2729fa2f3500dfc730fba1cd6027fdd1ede00d97
Ryan Bloom <rab83@duke.edu>
2019-04-08 19:14:35 -0400

    parse for action cards and action decks

--- src/Controller/ConfigReader.java
+++ src/Controller/ConfigReader.java
@@ -3,7 +3,6 @@ package Controller;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
@@ -18,10 +17,12 @@ public class ConfigReader {
     DocumentBuilder dBuilder;
     Document doc;
 
+    //https://www.tutorialspoint.com/java_xml/java_dom_parse_document.htm
     public ConfigReader(String filename) {
-        File inputFile = new File(filename);
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        //File inputFile = new File(filename);
         try {
+            File inputFile = new File(this.getClass().getClassLoader().getResource(filename).toURI());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             dBuilder = dbFactory.newDocumentBuilder();
             doc = dBuilder.parse(inputFile);
         }
@@ -30,28 +31,30 @@ public class ConfigReader {
         }
     }
 
-    public void parseActionDecks() throws XmlTagException{
+    public List<ActionDeck> parseActionDecks() throws XmlTagException{
         List<ActionDeck> decks = new ArrayList<>();
 
-        NodeList actionDeckList = doc.getElementsByTagName("ActionDecks");
+        NodeList actionDeckList = doc.getElementsByTagName("ActionDeck");
+
         for(int i=0; i<actionDeckList.getLength(); i++){
             Node ad = actionDeckList.item(i);
             if(ad.getNodeType() == Node.ELEMENT_NODE){
                 Element deck = (Element) ad;
-                String deckName = deck.getElementsByTagName("DeckType").item(0).getTextContent();
+                //String deckName = deck.getElementsByTagName("ActionDeck").item(0).getTextContent();
+                String deckName = deck.getTextContent();
                 DeckType dt = DeckType.valueOf(deckName);
                 ActionDeck tempDeck = new ActionDeck(dt);
                 decks.add(tempDeck);
             }
         }
-
+        return decks;
     }
 
-    public void parseActionCards() throws XmlTagException{
+    public List<AbstractActionCard> parseActionCards() throws XmlTagException{
         //Feed this allActionCards list into fillLiveDeck() in deck class after initializing empty decks
         List<AbstractActionCard> allActionCards = new ArrayList<>();
 
-        NodeList actionCardList = doc.getElementsByTagName("ActionCards");
+        NodeList actionCardList = doc.getElementsByTagName("ActionCardType");
         for(int i = 0; i<actionCardList.getLength(); i++){
             Node ac = actionCardList.item(i);
             if(ac.getNodeType() == Node.ELEMENT_NODE){
@@ -59,6 +62,7 @@ public class ConfigReader {
                 //All types of action cards have these fields
                 DeckType dt = DeckType.valueOf(card.getElementsByTagName("DeckType").item(0).getTextContent());
                 String msg = card.getElementsByTagName("Message").item(0).getTextContent();
+                //http://www.java67.com/2018/03/java-convert-string-to-boolean.html
                 Boolean holdable = Boolean.parseBoolean(card.getElementsByTagName("Holdable").item(0).getTextContent());
 
                 //Specialized fields below
@@ -87,12 +91,12 @@ public class ConfigReader {
                     AbstractActionCard newCard = new LoseMoneyAC(dt, msg, holdable, loseTo, amnt);
                     allActionCards.add(newCard);
                 }
-
                 else{
                     throw new XmlTagException(card.getAttribute("type"));
                 }
             }
         }
+        return allActionCards;
 
     }
 
@@ -115,10 +119,5 @@ public class ConfigReader {
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        ConfigReader c = new ConfigReader("/Users/SGGS1234/Desktop/workspace307/monopoly_team04/data/Normal_Config.xml");
-        c.parseSpaces();
     }
 }
