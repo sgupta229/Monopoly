23060419ef77f559f61ae77443259d0b305bafc2
DYLAN KARUL <djk41@duke.edu>
2019-04-08 00:59:25 -0400

    free parking space

---
+++ src/Model/FreeParkingSpace.java
@@ -0,0 +1,22 @@
+package Model;
+
+import Controller.AbstractGame;
+
+public class FreeParkingSpace extends AbstractSpace{
+
+    public FreeParkingSpace(int locationIndex, String spaceName){
+        super(locationIndex, spaceName);
+    }
+
+
+    /***
+     * This method performs the specific action that a type of space requires.
+     * It takes game in as a parameter so that it can do things such as
+     * figure out the current player, get the bank and perform bank actions,
+     * get a specific deck and draw a card, and more.
+     * @param game the active Game driver class for this game
+     */
+
+    public void doAction(AbstractGame game){
+    }
+}
