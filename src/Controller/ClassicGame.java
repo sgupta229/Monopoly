8d5334ed1261ccc7395044c96f78f4fc6858a2b2
Sahil Gupta <sg390@duke.edu>
2019-04-09 18:09:43 -0400

    removed and added occupant for spaces and called doAction method in classicGame

--- src/Controller/ClassicGame.java
+++ src/Controller/ClassicGame.java
@@ -1,10 +1,34 @@
 package Controller;
 
 
+import Model.AbstractSpace;
+
 public class ClassicGame extends AbstractGame {
 
     public ClassicGame(String filename) {
         super(filename);
     }
+
+    @Override
+    public int rollDice() {
+        int oldIndex = getCurrPlayer().getCurrentLocation();
+        AbstractSpace oldSpace = getBoard().getSpaceAt(oldIndex);
+        oldSpace.removeOccupant(getCurrPlayer());
+        int roll = super.rollDice();
+
+        if(!getCurrPlayer().isInJail()) {
+            getCurrPlayer().move(roll, getBoardSize());
+        }
+
+        int newIndex = getCurrPlayer().getCurrentLocation();
+        AbstractSpace newSpace = getBoard().getSpaceAt(newIndex);
+        newSpace.addOccupant(getCurrPlayer());
+
+        newSpace.doAction(this);
+
+        return roll;
+    }
+
+    public int
 
 }
