df39d15bbf1a87863b2a1de036bb2e72bc8b2535
Dezmanique Martin <dsm35@duke.edu>
2019-04-14 16:30:40 -0400

    loading and saving data

--- src/Controller/SaveData.java
+++ src/Controller/SaveData.java
@@ -1,4 +1,9 @@
 package Controller;
 
-public class SaveData {
+public class SaveData implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String name;
+    public int
 }
