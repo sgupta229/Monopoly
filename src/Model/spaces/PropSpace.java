999b59f7592c09a3435756b7b37974ea863ac18a
Caroline Breaux <cnb28@duke.edu>
2019-04-16 20:42:11 -0400

    fixed last dice roll that I had originally commented out, still works

--- src/Model/spaces/PropSpace.java
+++ src/Model/spaces/PropSpace.java
@@ -34,7 +34,7 @@ public class PropSpace extends AbstractSpace {
         AbstractPlayer currPlayer = game.getCurrPlayer();
         System.out.println("this is my property " + myProperty.getName());
         double propertyPrice = myProperty.getPrice();
-//        int lastDiceRoll = game.getLastDiceRoll();
+        int lastDiceRoll = game.getLastDiceRoll();
         if (propOwner == null) {
             if (userChoice == 0) { //aka buy property
                 bank.setPropertyOwner(myProperty, currPlayer);
@@ -48,7 +48,7 @@ public class PropSpace extends AbstractSpace {
         } else { //property is owned by someone, what are the choices?
             //pay rent or...
             System.out.println("PAYING RENT");
-            currPlayer.makePayment(myProperty.calculateRent(propOwner, 2), propOwner);
+            currPlayer.makePayment(myProperty.calculateRent(propOwner, lastDiceRoll), propOwner);
         }
         //else()
     }
