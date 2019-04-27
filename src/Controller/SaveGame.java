5903ceb29ca82711167e333205a65c519f2c5399
Sahil Gupta <sg390@duke.edu>
2019-04-27 12:11:37 -0400

    fixed bug in SaveGame class

--- src/Controller/SaveGame.java
+++ src/Controller/SaveGame.java
@@ -9,6 +9,9 @@ import Model.AbstractPlayer;
 import Model.ClassicPlayer;
 import Model.XmlReaderException;
 import Model.properties.Property;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -116,7 +119,7 @@ public class SaveGame {
         AbstractGame testGame = new ClassicGame(configFile);
         AbstractPlayer p1 = new ClassicPlayer("Dylan", "blah.img");
         AbstractPlayer p2 = new ClassicPlayer("Sahil", "testimage.img");
-        List<AbstractPlayer> players = new ArrayList<>();
+        ObservableList<AbstractPlayer> players = FXCollections.observableArrayList();
         players.add(p1);
         players.add(p2);
         testGame.setPlayers(players);
