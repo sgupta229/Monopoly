9d56cacecd4a5b06c0e3d04ff5006068be21eda3
DYLAN KARUL <djk41@duke.edu>
2019-04-27 12:46:55 -0400

    javadoc comments

--- src/Model/properties/JuniorColorProperty.java
+++ src/Model/properties/JuniorColorProperty.java
@@ -7,6 +7,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+
+/***
+ * Author: Dylan Karul
+ * Purpose: This class is used for all color properties in the junior game
+ * Assumptions: This is under the assumption that it gets its data in the right form
+ * but we make this assumption because we check for this in config reader when reading in
+ * the data, so it always should be getting proper data.
+ * Dependencies: It is dependent on AbstractPlayer,
+ * the BuildingType enum class, ColorProperty, and Property.
+ * How to use it: You would use this class by calling a new JuniorColorProperty to represent a
+ * color property in the junior rules game. Then, to get the rent info and other metadata,
+ * call getInfo. To see how much someone pays when landing on the space, call calculateRent.
+ */
 public class JuniorColorProperty extends ColorProperty {
 
     //private int numHouse;
@@ -20,15 +33,6 @@ public class JuniorColorProperty extends ColorProperty {
         getInfo();
     }
 
-    /*protected void initializePaymentInfo(List<Double> paymentInformation) throws IndexOutOfBoundsException{
-        List<Double> paymentInformationCopy = paymentInformation;
-
-            //pricePerHouse = paymentInformation.get(6);
-            //pricePerHotel = paymentInformation.get(7);
-            setMortgageAmount(paymentInformationCopy.get(paymentInformationCopy.size()-1));
-            rentNumbers = paymentInformationCopy.subList(0, paymentInformationCopy.size()-1);
-
-    }*/
 
     public List getInfo(){
         ret = new ArrayList();
@@ -39,14 +43,6 @@ public class JuniorColorProperty extends ColorProperty {
         return ret;
     }
 
-
-/*    *//***
-     * Allows for users to see the color of a property which is crucial for monopolies
-     * @return the color of this property
-     *//*
-    public String getPropertyColor(){
-        return myColor;
-    }*/
 
     /***
      * A method that utilizes the member variables to calculate how
@@ -64,15 +60,30 @@ public class JuniorColorProperty extends ColorProperty {
 
     }
 
+    /***
+     * Junior properties cannot build
+     * @param building the type of structure to build
+     * @throws UnsupportedOperationException
+     */
     public void addBuilding(BuildingType building) throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Junior Doesn't Build");
     }
 
+    /***
+     * Junior properties cannot build
+     * @param building the type of structure to build
+     * @throws UnsupportedOperationException
+     */
     public void removeBuilding(BuildingType building, int amount) throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Junior Doesn't Build");
 
     }
 
+    /***
+     * Junior properties cannot build
+     * @param building the type of structure to build
+     * @throws UnsupportedOperationException
+     */
     public int getNumBuilding(BuildingType building)throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Junior Doesn't Build");
         //return 0;
